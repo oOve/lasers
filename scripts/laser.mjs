605254ad3db9cb56614fa525d8d019d8483bb842
oOve <oOve@users.noreply.github.com>
2022-04-04 16:40:16 +0200

    minor error

--- scripts/laser.mjs
+++ scripts/laser.mjs
@@ -489,7 +489,7 @@ Hooks.once("init", () => {
     let los = wrapped(...args);
     
     // If this is a laser, modify the output
-    if (args[1].source.object?.data?.flags?.lasers?.is_laser){
+    if (args[1].source?.object?.data?.flags?.lasers?.is_laser){
       //console.log("Wrapped method los 'create' intercepted.");
       let rot = args[1].rotation;
       let rr = Math.toRadians(rot);
